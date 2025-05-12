72b177841cfee67db7f9585af017f6769f01e5bf
tmv3301 <162563703+tmv3301@users.noreply.github.com>
2025-05-12 22:01:55 +0900

    Update callstack_2023350225_권두현.cpp

--- callstack_2023350225_권두현.cpp
+++ callstack_2023350225_권두현.cpp
@@ -1,39 +1,47 @@
 #include <stdio.h>
 #define STACK_SIZE 50
 
-int call_stack[STACK_SIZE];
-char stack_info[STACK_SIZE][20];
+int     call_stack[STACK_SIZE];
+char    stack_info[STACK_SIZE][20];
 
-int SP = -1;
+int SP = -1; 
 int FP = -1;
 
-void push(int value, const char* info);
-int pop();
-void print_stack();
 void func1(int arg1, int arg2, int arg3);
 void func2(int arg1, int arg2);
 void func3(int arg1);
 
-void push(int value, const char* info)
-{
-    if (SP >= STACK_SIZE - 1) {
-        printf("ERROR : Stack Overflow\n");
-        return;
+void push(int value, const char* info) {
+    if (SP < STACK_SIZE-1) {
+        call_stack[++SP] = value;
+        snprintf(stack_info[SP], sizeof stack_info[SP], "%s", info);
     }
-    SP++;
-    call_stack[SP] = value;
-    snprintf(stack_info[SP], sizeof(stack_info[SP]), "%s", info);
 }
 
-int pop()
-{
-    if (SP == -1) {
-        printf("ERROR : Stack Underflow\n");
-        return -1;
+int pop() {
+    if (SP >= 0) {
+        int v = call_stack[SP];
+        call_stack[SP] = 0;
+        stack_info[SP][0] = '\0';
+        SP--;
+        return v;
     }
-    int value = call_stack[SP];
-    SP--;
-    return value;
+    return -1;
+}
+
+void push_return_address() {
+    push(-1, "Return Address");
+}
+
+void prologue(const char* func_name) {
+    push_return_address();
+    push(FP, func_name);
+    FP = SP;
+}
+
+void epilogue() {
+    FP = pop();
+    pop();
 }
 
 void print_stack()
@@ -45,7 +53,7 @@ void print_stack()
     }
 
     printf("====== Current Call Stack ======\n");
-
+    
     for (int i = SP; i >= 0; i--)
     {
         if (call_stack[i] != -1)
@@ -63,86 +71,71 @@ void print_stack()
     printf("================================\n\n");
 }
 
-void prologue()
-{
-    push(FP, "old EBP");
-    FP = SP;
-}
-
-void epilogue()
-{
-    SP = FP;
-    FP = pop();
-}
-
 void func1(int arg1, int arg2, int arg3)
 {
-    prologue();
-
     int var_1 = 100;
 
-    push(var_1, "var_1");
-    push(arg1, "arg1");
-    push(arg2, "arg2");
     push(arg3, "arg3");
+    push(arg2, "arg2");
+    push(arg1, "arg1");
+    prologue("func1 SFP");
+    push(var_1,"var_1");
 
     print_stack();
+
     func2(11, 13);
+	print_stack();
+
+    pop();       // var_1
+    epilogue();
+    pop();       // arg1
+    pop();       // arg2
+    pop();       // arg3
 
     print_stack();
-    pop();  // arg3
-    pop();  // arg2
-    pop();  // arg1
-    pop();  // var_1
-
-    epilogue();
 }
 
 void func2(int arg1, int arg2)
 {
-    prologue();
-
     int var_2 = 200;
 
-    push(var_2, "var_2");
-    push(arg1, "arg1");
     push(arg2, "arg2");
+    push(arg1, "arg1");
+    prologue("func2 SFP");
+    push(var_2, "var_2");
 
     print_stack();
+
     func3(77);
-
     print_stack();
-    pop();  // arg2
-    pop();  // arg1
-    pop();  // var_2
 
+    pop();
     epilogue();
+    pop();
+    pop();
 }
 
 void func3(int arg1)
 {
-    prologue();
-
     int var_3 = 300;
     int var_4 = 400;
 
+    push(arg1, "arg1");
+    prologue("func3 SFP");
     push(var_3, "var_3");
     push(var_4, "var_4");
-    push(arg1, "arg1");
 
     print_stack();
 
-    pop();  // arg1
-    pop();  // var_4
-    pop();  // var_3
-
+    pop();       // var_4
+    pop();       // var_3
     epilogue();
+    pop();       // arg1
 }
 
 int main()
 {
     func1(1, 2, 3);
-
-    print_stack();
+    //print_stack();
     return 0;
 }
